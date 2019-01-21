bc0a0479b5b422922cca2523caab60c56964782b
Dhanush <41576552+peddidhanush@users.noreply.github.com>
2019-01-21 17:12:08 +0530

    Delete posts.php

--- app/models/posts.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class posts extends BaseModel {
-        public function getPosts() {
-            $sql = "SELECT * FROM posts";
-            $this->query($sql);
-            return $this->resultSet();
-        }
-    }
