7a62ad7959e4e95a4a37461691615b2ccedc853c
Dhanush <41576552+peddidhanush@users.noreply.github.com>
2019-01-21 17:11:02 +0530

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,9 +3,7 @@
         public function index()
         {
             # code...
-            $this->posts = $this->model('posts');
-            $post = $this->posts->getPosts();
             // print_r($post);
-            $this->view('home/index.html.twig', ["posts"=>$post, "title" => "Dhanush"]);
+            $this->view('home/index.html.twig', ["title" => "Dhanush"]);
         }
     }
